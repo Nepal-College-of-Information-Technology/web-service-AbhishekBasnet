402a615ece79e92578bf24e4e16a2a32d52b21e6
Abhishek Man Basnet <abhishek.211703@ncit.edu.np>
2025-05-15 20:52:30 +0545

    JPA Repo clean ups

--- backEnd/HotelBooking/src/main/java/com/example/HotelBooking/repositories/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.HotelBooking.repositories;
-
-import com.example.HotelBooking.entities.User;
-import org.springframework.data.jpa.repository.JpaRepository;.
-
-public class UserRepository extends JpaRepository<User, Long> {
-}
