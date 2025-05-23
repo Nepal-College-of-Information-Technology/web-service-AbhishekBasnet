88b66cfed1e9482dcdd964f0eec999c399d471db
Abhishek Man Basnet <Abhishekmanbasnet@hotmail.com>
2025-05-23 14:46:39 +0545

    clean ups

--- backEnd/HotelBooking/src/main/java/com/example/HotelBooking/notification/NotificationService.java
+++
@@ -1,9 +0,0 @@
-package com.example.HotelBooking.notification;
-
-import com.example.HotelBooking.dtos.NotificationDTO;
-
-public interface NotificationService {
-    void sendEmail(NotificationDTO notificationDTO);
-    void sendSms();
-    void sendWhatsapp();
-}
